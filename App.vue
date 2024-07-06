fb5fc99b92dc5e81c63fa9c5d884a04e49ef77cb
amanhij <82947308+amanhij@users.noreply.github.com>
2024-07-06 00:18:47 -0700

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/expenses">Expenses</router-link></li>
+        <li><router-link to="/budgets">Budgets</router-link></li>
+        <li><router-link to="/savings">Savings</router-link></li>
+        <li><router-link to="/investments">Investments</router-link></li>
+      </ul>
+    </nav>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+/* Add any global styles */
+</style>
